250669599ba8321704dcf8f6f53e9a99ccdba7d7
Bernardo Lucchesi <ber.lucchesi@gmail.com>
2012-08-13 22:14:04 -0300

    Funcionario + Secretaria + Chefe

---
+++ entregas/Aula3/src/main/java/br/com/senac/brl/labprog/Aula3/Funcionario.java
@@ -0,0 +1,17 @@
+package br.com.senac.brl.labprog.Aula3;
+
+public class Funcionario extends Pessoa{
+	
+	private Integer id = 0;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+	
+}
---
+++ entregas/Aula3/src/main/java/br/com/senac/brl/labprog/Aula3/Secretaria.java
@@ -0,0 +1,5 @@
+package br.com.senac.brl.labprog.Aula3;
+
+public class Secretaria extends Funcionario{
+	
+}
