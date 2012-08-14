07761c74ceb7105b6238b4135cc5ed60e81b2ed1
Bernardo Lucchesi <ber.lucchesi@gmail.com>
2012-08-13 21:47:47 -0300

    Classe Pessoa adicionada com get e set.

---
+++ entregas/Aula3/src/main/java/br/com/senac/brl/labprog/Aula3/Pessoa.java
@@ -0,0 +1,17 @@
+package br.com.senac.brl.labprog.Aula3;
+
+public class Pessoa {
+
+	private String nome = null;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
